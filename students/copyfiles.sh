6c0b23508fef536e210394347a8dde87c78609d3
pravallikanagireddi <pravallika.nagireddi@gmail.com>
2022-05-27 14:33:25 +0530

    hello

---
+++ students/copyfiles.sh
@@ -0,0 +1,11 @@
+for file in `ls *.txt`;
+do
+foldername=`echo $file |awk -F. '{ print $1 }' `;
+if [ -d $foldername ];
+then rm -r $foldername;
+fi
+mkdir $foldername;
+cp $file $foldername;
+date %file
+done
+
